09b9fe000e0a02c14990559c75eb11c4d826cce1
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-02-24 15:53:05 +0800

    Add files via upload

--- Arduino-SDVX-Controller.ino
+++ Arduino-SDVX-Controller.ino
@@ -1,7 +1,7 @@
 #include <Keyboard.h>
 #include <Mouse.h>
 #include <Encoder.h>
-#define Keys 11
+
 typedef struct {
   int pin;
   char keycode;
@@ -9,17 +9,18 @@ typedef struct {
 
 LIST keylist[] =
 {
-  {9, 'q'}, {8, 'w'},
-  {0, 'o'}, {1, 'p'},
-  {7, 'd'}, {6, 'f'}, {3, 'j'}, {2, 'k'},
-  {5, 'g'}, {4, 'h'},
-  {10, KEY_RETURN},
+  //{9, 'q'}, {8, 'w'},
+  //{16, 'o'}, {10, 'p'},
+  {7, 'd'}, {6, 'f'}, {15, 'j'}, {14, 'k'},
+  {5, 'g'}, {18, 'h'},
+  {4, 'y'},
 };
 
-Encoder encoder1(14, 16);
-Encoder encoder2(15, 18);
-int old_ec1;
-int old_ec2;
+Encoder encoder1(3, 2);
+Encoder encoder2(0, 1);
+int o_ec1;
+int o_ec2;
+int Keys = sizeof(keylist) / sizeof(keylist[0]);
 
 void setup() {
   for (int i = 0; i < Keys; i++) {
@@ -27,8 +28,8 @@ void setup() {
   }
   Mouse.begin();
   Keyboard.begin();
-  old_ec1 = encoder1.read();
-  old_ec2 = encoder2.read();
+  o_ec1 = encoder1.read();
+  o_ec2 = encoder2.read();
 }
 
 void loop() {
@@ -38,15 +39,15 @@ void loop() {
     } else {
       Keyboard.release(keylist[i].keycode);
     }
-    int ec1 = encoder1.read();
-    int ec2 = encoder2.read();
-    if (ec1 != old_ec1) {
-      Mouse.move(ec1 > old_ec1 ? -5 : 5, 0);
-      old_ec1 = ec1;
-    }
-    if (ec2 != old_ec2) {
-      Mouse.move(0, ec2 > old_ec2 ? -5 : 5);
-      old_ec2 = ec2;
-    }
+  }
+  int ec1 = encoder1.read();
+  int ec2 = encoder2.read();
+  if (ec1 != o_ec1) {
+    Mouse.move(ec1 > o_ec1 ? -5 : 5, 0);
+    o_ec1 = ec1;
+  }
+  if (ec2 != o_ec2) {
+    Mouse.move(0, ec2 > o_ec2 ? -5 : 5);
+    o_ec2 = ec2;
   }
 }
