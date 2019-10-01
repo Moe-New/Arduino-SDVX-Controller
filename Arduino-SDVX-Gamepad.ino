c658f3a8712a67354bb140fcab75fa5b798e6b95
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2019-10-01 17:48:45 +0800

    将旋钮模拟的鼠标替换为摇杆。

--- Arduino-SDVX-Gamepad.ino
+++ Arduino-SDVX-Gamepad.ino
@@ -1,37 +1,39 @@
-#include <Mouse.h>
 #include <Joystick.h>
 #include <Encoder.h>
+
+const int keylist[] =
+{
+  4,
+  7, 6, 15, 14,
+  5, 18,
+};
+const int Keys = sizeof(keylist) / sizeof(keylist[0]);
+
 Joystick_ Joystick(
   JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
-  7, 0,
-  false, false, false,
+  Keys, 0,
+  true, true, false,
   false, false, false,
   false, false,
   false, false, false
 );
 
-const int keylist[] =
-{
-  7, 6, 15, 14,
-  5, 18,
-  4,
-};
-const int Keys = sizeof(keylist) / sizeof(keylist[0]);
-const int P = 3;
-
 Encoder encoder1(2, 3);
 Encoder encoder2(20, 19);
+
 
 void setup() {
   for (int i = 0; i < Keys; i++) {
     pinMode(keylist[i], INPUT_PULLUP);
   }
-  Mouse.begin();
+  Joystick.setXAxisRange(-64, 63);
+  Joystick.setYAxisRange(-64, 63);
   Joystick.begin();
 }
 boolean lastStatus[Keys];
 int o_ec1 = encoder1.read();
 int o_ec2 = encoder2.read();
+
 
 void loop() {
   for (int i = 0; i < Keys; i++) {
@@ -42,13 +44,28 @@ void loop() {
     }
   }
   int ec1 = encoder1.read();
-  int ec2 = encoder2.read();
   if (ec1 != o_ec1) {
-    Mouse.move(ec1 < o_ec1 ? -P : P, 0);
+        if (ec1 > 63) {
+          ec1 -= 128;
+          encoder1.write(ec1);
+        } else if (ec1 < -64) {
+          ec1 += 128;
+          encoder1.write(ec1);
+        }
+    Joystick.setXAxis(ec1);
     o_ec1 = ec1;
   }
+
+  int ec2 = encoder2.read();
   if (ec2 != o_ec2) {
-    Mouse.move(0, ec2 < o_ec2 ? -P : P);
+    if (ec2 > 63) {
+      ec2 -= 128;
+      encoder2.write(ec2);
+    } else if (ec2 < -64) {
+      ec2 += 128;
+      encoder2.write(ec2);
+    }
+    Joystick.setYAxis(ec2);
     o_ec2 = ec2;
   }
 }
